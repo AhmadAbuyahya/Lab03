c212b57ceaace0135fc98118c52c65c254b542d8
AhmadAbuyahya <Ahmadabuyahya998@gmail.com>
2021-01-28 13:09:12 +0200

    added lab05

--- lab05.js
+++ lab05.js
@@ -1,1 +1,20 @@
+var product = prompt("Would you like to buy a phone or a laptop?");
 
+while (product !== "phone" && product !== "laptop") {
+    product = prompt("Would you like to buy a phone or a laptop?");
+}
+
+var image = ''
+if(product==="phone"){
+image='<img src = "images/phone.png"/>';
+}
+else if(product==="laptop"){
+    image='<img src="images/laptop.png"/>';
+}
+var number=prompt("How many would you like to order?")
+var result='';
+
+for (var i=0; i<number;i++){
+    result=result+image
+}
+document.write(result);
